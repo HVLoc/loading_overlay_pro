b6d1086d334e33560a91e51e8e759a175c30af07
lochv <lochv@intern.softdreams.vn>
2020-06-26 11:43:59 +0700

    update code

---
+++ example/android/app/src/main/java/vn/softdreams/loading_overlay_pro/MainActivity.java
@@ -0,0 +1,6 @@
+package vn.softdreams.loading_overlay_pro;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/src/src.dart
@@ -0,0 +1,11 @@
+library loading_overlay_pro;
+
+export 'flipping_shape.dart';
+export 'rotating_shape.dart';
+export 'double_flipping_shape.dart';
+export 'bouncing_grid.dart';
+export 'filling_shape.dart';
+export 'jumping_line.dart';
+export 'bouncing_line.dart';
+export 'fading_line.dart';
+export 'bumping_line.dart';
