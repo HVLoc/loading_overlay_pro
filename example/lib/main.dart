4faa9d3f1788b604a7a358f0fd7e60892b3ba5df
lochv <lochv@intern.softdreams.vn>
2020-10-24 15:20:16 +0700

    fix file example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:example/percentage_example.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'loading_example.dart';
+import 'package:loading_overlay_pro/loading_overlay_pro.dart';
 
 void main() => runApp(LoadingApp());
 
@@ -65,4 +63,162 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+}
+
+class LoadingOverlayPage extends StatefulWidget {
+  @override
+  _LoadingOverlayPageState createState() => _LoadingOverlayPageState();
+}
+
+class _LoadingOverlayPageState extends State<LoadingOverlayPage> {
+  bool _isLoading = false;
+
+  bool _isIOS = false;
+
+  Duration duration = Duration(seconds: 3);
+
+  void _submit() {
+    setState(() {
+      _isLoading = true;
+    });
+
+    Future.delayed(duration, () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Loading Overlay'),
+          backgroundColor: Colors.blue,
+        ),
+        body: LoadingOverlayPro(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RaisedButton(
+                  onPressed: () {
+                    _isIOS = false;
+                    _submit();
+                  },
+                  child: Text('Show Loading BouncingLine'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    _isIOS = true;
+                    _submit();
+                  },
+                  child: Text('Show Loading Custom IOS'),
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: _isIOS ? Colors.white : Colors.black54,
+          isLoading: _isLoading,
+          progressIndicator: _isIOS
+              ? CupertinoActivityIndicator(radius: 100)
+              : LoadingBouncingLine.circle(
+                  backgroundColor: Colors.blue,
+                  size: 150.0,
+                  duration: Duration(seconds: 2),
+                  borderColor: Colors.blue,
+                ),
+          overLoading: _isIOS
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FlutterLogo(),
+                    SizedBox(width: 10),
+                    Text(
+                      "Loading Overlay Pro",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                )
+              : null,
+          bottomLoading: _isIOS
+              ? Text("Loading...", style: TextStyle(fontSize: 20.0))
+              : null,
+        ),
+      );
+
+  Widget test() {
+    return LoadingOverlayPro(
+      child: Center(
+        child: RaisedButton(
+          onPressed: () {
+            _submit();
+          },
+          child: Text('Show Loading Custom IOS'),
+        ),
+      ),
+      backgroundColor: _isIOS ? Colors.white : Colors.black54,
+      isLoading: _isLoading,
+      progressIndicator: _isIOS
+          ? CupertinoActivityIndicator(radius: 100)
+          : LoadingBouncingLine.circle(
+              backgroundColor: Colors.blue,
+              size: 150.0,
+              duration: Duration(seconds: 2),
+              borderColor: Colors.blue,
+            ),
+      overLoading: Text("App Name"),
+      bottomLoading: Text("Loading..."),
+    );
+  }
+}
+
+class PercentPage extends StatefulWidget {
+  @override
+  _PercentPageState createState() => _PercentPageState();
+}
+
+class _PercentPageState extends State<PercentPage> {
+  bool _isLoading = false;
+
+  Duration duration = Duration(seconds: 10);
+
+  void _submit() {
+    setState(() {
+      _isLoading = true;
+    });
+
+    Future.delayed(duration, () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Loading Percent'),
+          backgroundColor: Colors.blue,
+        ),
+        body: LoadingPercentage(
+          timer: duration,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RaisedButton(
+                  onPressed: () {
+                    _submit();
+                  },
+                  child: Text('Show Loading Download 10s'),
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: Colors.black54,
+          isLoading: _isLoading,
+        ),
+      );
 }
